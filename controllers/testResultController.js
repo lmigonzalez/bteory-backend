e97e3fb03db54f24e2340994c313eee2355ab76d
joszze <jossecarlos@gmail.com>
2023-06-10 16:07:12 -0400

    fix test flow

--- controllers/testResultController.js
+++ controllers/testResultController.js
@@ -1,10 +1,10 @@
-const TestResult = require('../models/testResultModel');
-const Test = require('../models/customTestModel');
-const Questions = require('../models/questionModel');
+const TestResult = require("../models/testResultModel");
+const Test = require("../models/customTestModel");
+const Questions = require("../models/questionModel");
 
 const createTestResult = async (req, res) => {
   const { testId, answers } = req.body;
-  const userId = '1234567890';
+  const userId = "1234567890";
 
   try {
     const test = await Test.findById(testId);
@@ -13,12 +13,19 @@ const createTestResult = async (req, res) => {
       _id: { $in: testQuestionIdArray },
     });
 
-    const result = foundQuestions.map((question, index) => {
-      const questionId = question._id.toString();
-      const questionAnswer = sanitizeAnswer(question.answer);
-      const userAnswer = sanitizeAnswer(answers[index]);
-      const isCorrect = compareAnswers(questionAnswer, userAnswer);
-      return { questionId, questionAnswer, userAnswer, isCorrect };
+    result = answers.map((item) => {
+      const { id, userAnswer } = PrepareAnswer(item);
+      const question = foundQuestions.find(
+        (question) => question._id.toString() === id
+      );
+      const isCorrect = userAnswer === question.answer;
+
+      return {
+        questionId: question._id,
+        questionAnswer: question.answer,
+        userAnswer: userAnswer,
+        isCorrect: isCorrect,
+      };
     });
 
     const newTestResult = new TestResult({
@@ -29,7 +36,7 @@ const createTestResult = async (req, res) => {
       complexity: test.complexity,
     });
 
-	console.log(result)
+    console.log(result);
 
     await newTestResult.save();
 
@@ -40,10 +47,17 @@ const createTestResult = async (req, res) => {
   }
 };
 
+function PrepareAnswer(answer) {
+  return {
+    id: answer.split("-")[0],
+    userAnswer: answer.slice(answer.indexOf("-") + 1),
+  };
+}
+
 const sanitizeAnswer = (answer) => {
   let sanitizedAnswer = answer.trim();
 
-  if (sanitizedAnswer.endsWith('.')) {
+  if (sanitizedAnswer.endsWith(".")) {
     sanitizedAnswer = sanitizedAnswer.slice(0, -1);
   }
 
